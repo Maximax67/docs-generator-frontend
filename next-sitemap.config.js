03a943fd4c8ded675ac45b86c8bbb2d987aeafe9
Bielikov Maksym <maximax6767@gmail.com>
2025-10-06 20:17:35 +0300

    seo: add robots.txt and sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  generateRobotsTxt: true,
+  outDir: `${__dirname}/out`,
+  exclude: [
+    '/login',
+    '/logout',
+    '/register',
+    '/reset-password',
+    '/verify-email',
+    '/profile',
+    '/users',
+    '/documents/result',
+  ],
+  changefreq: 'monthly',
+  priority: 0.7,
+};
